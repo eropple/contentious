be08a9deefbaa7236b27e5583d350fdbba4d30f5
Ed Ropple <ed@edropple.com>
2012-04-29 21:19:50 -0400

    Added Ed.Contentious.XnaWindows project.

---
+++ src/Ed.Contentious.Xna/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ed.Contentious.XnaWindows")]
+[assembly: AssemblyProduct("Ed.Contentious.XnaWindows")]
+[assembly: AssemblyDescription("Contentious extensions for XNA on Windows.")]
+[assembly: AssemblyCompany("http://github.com/eropple/contentious")]
+[assembly: AssemblyCopyright("Copyright © Ed Ropple 2012; released under the ISC license.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("7bf1224f-9145-44bc-8313-9b7970d02273")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
